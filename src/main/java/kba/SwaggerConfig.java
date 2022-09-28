fdd07680588f2ac13e5fa6295bd6c050754c30ab
Yanis <rida.yanis@outlook.fr>
2022-09-28 15:52:15 +0200

    setting up project folders and swagger

---
+++ src/main/java/kba/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package kba;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build().apiInfo(apiEndPointsInfo());
+    }
+
+    private ApiInfo apiEndPointsInfo() {
+        return new ApiInfoBuilder().title("Kata Bank Account API")
+                .description("Demo API for the Kata Bank Account technical assessment")
+                .contact(new Contact("Yanis RIDA", "", "rida.yanis@outlook.fr"))
+                .version("1.0.0")
+                .build();
+    }
+
+}
